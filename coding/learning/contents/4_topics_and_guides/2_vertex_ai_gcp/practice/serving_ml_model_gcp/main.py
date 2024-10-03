2da94af8585bc98ce9c618739af4e4387891796d
datkt <datkt@pinetree.vn>
2024-10-03 19:04:14 +0700

    update

--- coding/learning/contents/4_topics_and_guides/2_vertex_ai_gcp/practice/serving_ml_model_gcp/main.py
+++ coding/learning/contents/4_topics_and_guides/2_vertex_ai_gcp/practice/serving_ml_model_gcp/main.py
@@ -70,6 +70,7 @@ async def predict(request: Request):
     # Return the predictions
     return output
 
+
 # Main function to run the FastAPI app
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
